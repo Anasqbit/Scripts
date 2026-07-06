4ffcb066adac515879de14fd7c3a01f1bfb5d768
Anasqbit <196997813+Anasqbit@users.noreply.github.com>
2026-07-07 02:55:13 +0300

    es

--- INSTA DOWNLOADER v2.2.js
+++ INSTA DOWNLOADER v2.2.js
@@ -7,7 +7,7 @@
 // @name:es Descargador de Instagram
 // @name:fr Téléchargeur Instagram
 // @name:ru Загрузчик Instagram
-// @version v2.2
+// @version v1.2
 // @compatible chrome
 // @description Add the download button to download profile picture and media in the posts, stories, and highlights in Instagram
 // @author Anasqbit
